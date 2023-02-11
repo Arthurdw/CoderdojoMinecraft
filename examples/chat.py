a9fa60f04103e5391a7fe9c9561a028ef768be9c
Arthurdw <dev@arthurdw.com>
2023-02-11 10:58:24 +0100

    :tada: Initial publish

---
+++ examples/chat.py
@@ -0,0 +1,23 @@
+from cdj_minecraft.minecraft import Bot, waneer
+
+bot = Bot(naam="robot")
+
+
+@waneer("bericht")
+def bericht(gebruikersnaam, message):
+    if gebruikersnaam == bot.naam:
+        return
+
+    print(f"{gebruikersnaam}: {message}")
+
+    if message.trim().lower() == "hallo":
+        bot.zeg(f"Hallo {gebruikersnaam}!")
+
+
+@waneer("login")
+def login():
+    print(f"Ingelogd op {bot.server} als {bot.naam}.")
+    bot.zeg(f"Hallo, ik ben {bot.naam}!")
+
+
+bot.start()
